b9b7318ced0fa3209424f909a627f80b1476184c
Ric Claus <claus@slac.stanford.edu>
2025-12-06 11:37:17 -0800

    Add setup_env.sh file

---
+++ setup_env.sh
@@ -0,0 +1,7 @@
+#  Source this script prior to building the LCLS2 DAQ
+
+RELDIR="$( cd "$( dirname $(readlink -f "${BASH_SOURCE[0]:-${(%):-%x}}") )" && pwd )"
+export LC_DIR=$RELDIR/install/cmake
+export PFPL_DIR=$RELDIR/install/cmake
+export CUSZ_DIR=$RELDIR/install/lib64/cmake/CUSZ
+export cuSZp_DIR=$RELDIR/install/cmake
